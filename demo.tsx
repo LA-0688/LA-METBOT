4451d4665ffc6d65dc676c76894aee776b44566f
LA-0688 <LA-0688@users.noreply.github.com>
2026-05-27 17:20:37 +0530

    Add animated weather icons and dynamic popup card icons

---
+++ demo.tsx
@@ -0,0 +1,9 @@
+import { Component } from "@/components/ui/animated-weather-icons";
+
+export default function Demo() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-background">
+      <Component />
+    </div>
+  );
+}
