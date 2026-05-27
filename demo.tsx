e9e3ec2513bb96616e6b5e456434e3f67587f5be
LA-0688 <LA-0688@users.noreply.github.com>
2026-05-27 17:49:26 +0530

    Remove unused React TSX files that may be confusing Render's buildpack autodetection

--- demo.tsx
+++
@@ -1,9 +0,0 @@
-import { Component } from "@/components/ui/animated-weather-icons";
-
-export default function Demo() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-background">
-      <Component />
-    </div>
-  );
-}
